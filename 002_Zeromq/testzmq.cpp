1fa3f72b67b6b5192b63be9909b3b6ff9271910b
ec06cumt <lgcumt2008@gmail.com>
2016-02-23 16:01:45 +0800

    Added files via upload

---
+++ 002_Zeromq/testzmq.cpp
@@ -0,0 +1,60 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "zmq.h"
+#include "zmq_utils.h"
+
+int main(int argc,char* *argv)
+{
+
+    void* pCtx = NULL;
+    void* pSock = NULL;
+    const char* pAddr = "tcp://*:7788";
+    //create context,zmq socket 
+    if((pCtx = zmq_ctx_new()) == NULL)
+    {
+       return 0;
+    }
+    
+    if((pSock = zmq_socket(pCtx,ZMQ_DEALER)) == NULL)
+    {
+       zmq_ctx_destroy(pCtx);
+       return 0;
+    }
+
+    int iRcvTimeout = 5000;    //millsecond
+
+    //set timeout 5s
+    if(zmq_setsockopt(pSock,ZMQ_RCVTIMEO,&iRcvTimeout,sizeof(iRcvTimeout)) < 0)
+    {
+      zmq_close(pSock);
+      zmq_ctx_destroy(pCtx);
+      return 0;
+    }
+
+    //bind ipaddress port:7788
+    if(zmq_bind(pSock,pAddr) < 0)
+    {
+      zmq_close(pSock);
+      zmq_ctx_destroy(pCtx);
+      return 0;
+    }
+    printf("bind at:%s\n",pAddr);
+    while(1)
+    {
+      char szMsg[1024] = {0};
+      printf("waitting...\n");
+      
+
+      if(zmq_recv(pSock,szMsg,sizeof(szMsg),0) < 0)
+      {
+	
+        continue;
+      }
+
+      printf("received message : %s\n",szMsg);
+    }
+
+    printf("argc=%d\n",argc);
+    printf("test zmq project!\n");
+    return 0;
+}
